32c57938a764fb1efeb02c47de0a9f04fe204802
Polo <pablo_mo@orange.fr>
2022-12-02 11:37:27 -0500

    Seed Admin

---
+++ api/src/main/java/com/uqam/api/model/seed/SeedAdmin.java
@@ -0,0 +1,27 @@
+package com.uqam.api.model.seed;
+
+import com.uqam.api.model.dao.AdministratorDAO;
+import com.uqam.api.model.entity.Administrator;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeedAdmin implements CommandLineRunner {
+    private final AdministratorDAO administratorDAO;
+    private final PasswordEncoder passwordEncoder;
+
+    public SeedAdmin(AdministratorDAO administratorDAO, PasswordEncoder passwordEncoder) {
+        this.administratorDAO = administratorDAO;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedAdmin();
+    }
+
+    private void seedAdmin() {
+        administratorDAO.save(new Administrator("Admin", "Admin", "admin@admin.ca", passwordEncoder.encode("secret")));
+    }
+}
