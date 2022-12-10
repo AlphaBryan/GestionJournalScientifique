d99e6882f1bd029bda11440267484aa9e986fec4
alphabryan <90648570+AlphaBryan@users.noreply.github.com>
2022-12-10 18:24:00 -0500

    .

--- web/src/redux/features/edition/edition-slice.ts
+++ web/src/redux/features/edition/edition-slice.ts
@@ -1,61 +1,63 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {get, handleHttpErrors, httpJson, post} from "../../httpUtil";
-import {Edition} from "../../dto/Edition";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { get, handleHttpErrors, httpJson, post } from "../../httpUtil";
+import { Edition } from "../../dto/Edition";
 
 type CreateEdition = {
-    name: string;
-    submissionLimitDate: number;
-}
+  name: string;
+  submissionLimitDate: number;
+};
 
 export interface EditionSlice {
-    editions: Edition[];
+  editions: Edition[];
 }
 
 const initialState: EditionSlice = {
-    editions: [],
+  editions: [],
 };
 
-export const getEditions = createAsyncThunk(
-    'editions/get',
-    async () => {
-        const res = await httpJson(handleHttpErrors(get('/editions/')));
-        return res;
-    }
-);
+export const getEditions = createAsyncThunk("editions/get", async () => {
+  console.log("getEditions");
+  const res = await httpJson(handleHttpErrors(get("/editions/")));
+  return res;
+});
 export const addEdition = createAsyncThunk(
-    'editions/add',
-    async (edition: CreateEdition) => {
-        const res = await httpJson(handleHttpErrors(post('/editions/', edition)));
-        return res;
-    }
+  "editions/add",
+  async (edition: CreateEdition) => {
+    const res = await httpJson(handleHttpErrors(post("/editions/", edition)));
+    return res;
+  }
 );
 
 export const getEditionArticles = createAsyncThunk(
-    'editions/get-articles',
-    async (editionId: number) => {
-        const res = await httpJson(handleHttpErrors(get(`/articles/edition/${editionId}/articles`)));
-        return {editionId, data: res};
-    }
-)
+  "editions/get-articles",
+  async (editionId: number) => {
+    const res = await httpJson(
+      handleHttpErrors(get(`/articles/edition/${editionId}/articles`))
+    );
+    return { editionId, data: res };
+  }
+);
 
 export const editionSlice = createSlice({
-    name: 'edition',
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        builder.addCase(getEditions.fulfilled, (state, action) => {
-            state.editions = action.payload;
-        });
-        builder.addCase(addEdition.fulfilled, (state, action) => {
-            state.editions.push(action.payload);
-        });
-        builder.addCase(getEditionArticles.fulfilled, (state, action) => {
-            const edition = state.editions.find(e => e.id === action.payload.editionId);
-            if (edition) {
-                edition.articles = action.payload.data;
-            }
-        });
-    }
-})
+  name: "edition",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(getEditions.fulfilled, (state, action) => {
+      state.editions = action.payload;
+    });
+    builder.addCase(addEdition.fulfilled, (state, action) => {
+      state.editions.push(action.payload);
+    });
+    builder.addCase(getEditionArticles.fulfilled, (state, action) => {
+      const edition = state.editions.find(
+        (e) => e.id === action.payload.editionId
+      );
+      if (edition) {
+        edition.articles = action.payload.data;
+      }
+    });
+  },
+});
 
 export default editionSlice.reducer;
